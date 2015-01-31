dfc332286f85eda973eaa08f15b1aa5634b0a66d
Nguyen Ba Truong <truongnguyen1912@gmail.com>
2015-02-01 00:06:08 +0700

    Re-structure folders

---
+++ src/app/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appstructureApp
+ * @description
+ * # appstructureApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appstructureApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/user/views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'app/user/views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
